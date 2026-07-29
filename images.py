4c6121a64685773031cda8e00fb085f62e965276
Abeer Agarwal <2006abeer.agarwal@gmail.com>
2026-07-29 23:56:57 +0530

    Auto: update blog content and images

--- images.py
+++ images.py
@@ -3,9 +3,9 @@ import re
 import shutil
 
 # Paths (using raw strings to handle Windows backslashes correctly)
-posts_dir = r"C:\Users\2006a\OneDrive\Documentos\abeerblogs\content\docs"
+posts_dir = r"C:\Users\2006a\abeerblogs\content\docs"
 attachments_dir = r"C:\Users\2006a\OneDrive\Documentos\Obsidian\ZettelKasten\Assests\Images"
-static_images_dir = r"C:\Users\2006a\OneDrive\Documentos\abeerblogs\static\images"
+static_images_dir = r"C:\Users\2006a\abeerblogs\static\images"
 
 # Step 1: Process each markdown file in the posts directory
 for filename in os.listdir(posts_dir):
